502e33044a35fcc2bbc2bab7919ee38c9d10023c
Eric Zorn <ericzorndesigns@gmail.com>
2018-07-04 13:16:11 -0400

    Chatroom Completed

--- public/chat.js
+++ public/chat.js
@@ -6,6 +6,7 @@ const message = document.querySelector("#message");
 const handle = document.querySelector("#handle");
 const btn = document.querySelector("#send");
 const output = document.querySelector("#output");
+const feedback = document.querySelector("#feedback");
 
 // Emmit Events
 btn.addEventListener('click', () => {
@@ -13,4 +14,18 @@ btn.addEventListener('click', () => {
         message: message.value,
         handle: handle.value
     });
+});
+
+message.addEventListener('keypress', () => {
+    socket.emit('typing', handle.value);
+});
+
+// Listen for events
+socket.on('chat', (data) => {
+    output.innerHTML += `<p><strong>${data.handle}: </strong> ${data.message}`;
+    feedback.innerHTML = "";
+});
+
+socket.on('typing', data => {
+    feedback.innerHTML = `<p><em>${data} is typing a message...</em></p>`;
 });
